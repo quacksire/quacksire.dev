1398a5066b6d75806ae0c069ff2909fbfdc667e1
child_duckling <sam@quacksire.dev>
2023-02-20 01:18:01 -0800

    escapy

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+// 1. import `NextUIProvider` component
+import {createTheme, NextUIProvider} from '@nextui-org/react';
+
+const lightTheme = createTheme({
+    type: 'light',
+})
+
+const darkTheme = createTheme({
+    type: 'dark',
+})
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        // 2. Use at the root of your app
+        <NextUIProvider theme={darkTheme}>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
